3de3c67068c2cd4904f964d1fc0fea2dca0e570d
Wade Helquist <sirwade83@gmail.com>
2021-12-21 21:25:49 -0700

    New Keys & Display Panes
    Created option for new game on hitting ENTER
    Game over message / Empty High Score column to Left with title
    Moved Info to bottom out of way of play
    Display on bottom " STRATA SNAKE 🐍         Speed: 0  Per Apple: 100  Score:  0"
    Higher Per Apple given exponentially for faster speeds
    Higher Per apple for More body segments
    Ability to pause game with SPACE BAR
    Larger Playing area

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -4,32 +4,35 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.io.IOException;
 import java.text.DecimalFormat;
 import java.util.Random;
 
 public class GamePanel extends JPanel implements ActionListener {
 
-    static final int SCREEN_WIDTH = 600;
+    static final int SCREEN_WIDTH = 1200;
+    static final int SIDE_FOR_SCORE = 150;
+    static final int BOTTOM_PANEL = 100;
     static final int SCREEN_HEIGHT = 600;
     static final int UNIT_SIZE = 25;
     static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT) / UNIT_SIZE;
-    final int[] x = new int[GAME_UNITS];
-    final int[] y = new int[GAME_UNITS];
-    int speed = 100; //How fast game is running
-    float scoreMultiplierFloat = 100.0f;
+    int[] x;
+    int[] y;
+    int speed; //How fast game is running
+    float scoreMultiplierFloat;
     int scoreMultiplierInt;
     int score;
-    int snegyBodyParts = 6;
+    int snegyBodyParts;
     int appleX;
     int appleY;
-    char direction = 'D';
-    boolean running = false;
+    char direction;
+    boolean running;
     Timer timer;
     Random random;
 
     GamePanel() {
         random = new Random();
-        this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH + SIDE_FOR_SCORE, SCREEN_HEIGHT + BOTTOM_PANEL));
         this.setBackground(Color.black);
         this.setFocusable(true);
         this.addKeyListener(new MyKeyAdapter());
@@ -37,40 +40,56 @@ public class GamePanel extends JPanel implements ActionListener {
     }
 
     public void startGame() {
-        newApple();
+        direction = 'D';
+        snegyBodyParts = 6;
+        speed = 100;
+        score = 0;
         running = true;
+        scoreMultiplierFloat = 100.0f;
+        x = new int[GAME_UNITS];
+        y = new int[GAME_UNITS];
+        newApple();
         timer = new Timer(speed, this);
         timer.start();
     }
 
     public void newSpeed() {
         if(running) {
-            timer.stop();
-            timer.setDelay(speed);
-            timer.start();
+        timer.stop();
+        timer.setDelay(speed);
+        timer.start();
         }
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         draw(g);
+        if (!running) {
+
+            //set font size and color
+            g.setColor(Color.green);
+            g.setFont(new Font("TimesRoman", Font.PLAIN, UNIT_SIZE * 2));
+
+            //show GAME OVER
+            g.drawString("GAME OVER", SCREEN_WIDTH / 3, SCREEN_HEIGHT / 2);
+
+        }
     }
 
     public void draw(Graphics g) {
-        for (int i = 0; i < SCREEN_HEIGHT / UNIT_SIZE; i++) {
+
+        for (int i = 0; i <= SCREEN_WIDTH / UNIT_SIZE; i++) {
             g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT);
-            g.drawLine(0, i * UNIT_SIZE, SCREEN_HEIGHT, i * UNIT_SIZE);
+        }
+        for (int i = 0; i <= SCREEN_HEIGHT / UNIT_SIZE; i++) {
+            g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i * UNIT_SIZE);
         }
 
-        scoreMultiplierInt = (int) scoreMultiplierFloat;
-        g.setColor(Color.green);
-        g.setFont(new Font("TimesRoman", Font.PLAIN, UNIT_SIZE * 2));
-        g.drawString(String.format("%d", scoreMultiplierInt), UNIT_SIZE , UNIT_SIZE * 2); //show score multiplier
-        DecimalFormat scoreWithCommas = new DecimalFormat("#,###");
-        g.drawString(scoreWithCommas.format(score), SCREEN_WIDTH / 2 , UNIT_SIZE * 2); //show score multiplier
+        //set apple
         g.setColor(Color.red);
         g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
 
+        // set snake named SNEGY
         for (int i = 0; i < snegyBodyParts; i++) {
             if (i == 0) {
                 g.setColor(Color.green);
@@ -81,13 +100,37 @@ public class GamePanel extends JPanel implements ActionListener {
             }
         }
 
-//        g.setColor(Color.green);
+        //fill in bottom
+        g.setColor(Color.darkGray);
+        g.fillRect(0, SCREEN_HEIGHT, SCREEN_WIDTH + SIDE_FOR_SCORE, BOTTOM_PANEL);
+        g.setFont(new Font("TimesRoman", Font.PLAIN, UNIT_SIZE));
+
+        //show the per apple amount and score at bottom
+        scoreMultiplierInt = (int) scoreMultiplierFloat;
+        g.setColor(Color.green);
+        g.setFont(new Font("TimesRoman", Font.PLAIN, (int) (UNIT_SIZE * 1.5)));
+        DecimalFormat scoreWithCommas = new DecimalFormat("#,###");
+        g.drawString("       StrataSnake \uD83D\uDC0D", 0, SCREEN_HEIGHT + (BOTTOM_PANEL / 2) + 15);
+        g.drawString("Speed: " + (100 - speed) / 5 + "   Per Apple: " + scoreWithCommas.format(scoreMultiplierInt) +
+                "   Score: " + scoreWithCommas.format(score), SCREEN_WIDTH / 3, SCREEN_HEIGHT
+                + +(BOTTOM_PANEL / 2) + 15);
+
+
+        //set side for high score table
+        g.setColor(Color.lightGray);
+        g.fillRect(SCREEN_WIDTH, 0, SCREEN_WIDTH + SIDE_FOR_SCORE, SCREEN_HEIGHT);
+        g.setColor(Color.black);
+        g.setFont(new Font("TimesRoman", Font.PLAIN, UNIT_SIZE * 3 / 4));
+        g.drawString("High Scores:", SCREEN_WIDTH + (SIDE_FOR_SCORE / 8), UNIT_SIZE);
+
+        //Used later for numbers one red one green - to make game strategic
 //        newApple();
-//        g.setFont(new Font("TimesRoman", Font.PLAIN, UNIT_SIZE));
+//        g.setColor(Color.green);
 //        g.drawString(String.valueOf(1), appleX + (int) (UNIT_SIZE / 5), (int) appleY + (UNIT_SIZE * 5/6 ));
 //        newApple();
 //        g.setColor(Color.red);
 //        g.drawString(String.valueOf(1), appleX + (int) (UNIT_SIZE / 5), (int) appleY + (UNIT_SIZE * 5/6 ));
+
     }
 
     public void newApple() {
@@ -109,8 +152,10 @@ public class GamePanel extends JPanel implements ActionListener {
     }
 
     public void checkApple() {
-        if( ( x[0] == appleX ) && (y[0] == appleY) ) {
+        //If eats apple
+        if ((x[0] == appleX) && (y[0] == appleY)) {
             snegyBodyParts++;
+            scoreMultiplierFloat *= 1.035333f;
             score += scoreMultiplierInt;
             newApple();
         }
@@ -126,7 +171,7 @@ public class GamePanel extends JPanel implements ActionListener {
                 running = false;
             }
             //check if head touches right border
-            if (x[0] > SCREEN_WIDTH) {
+            if (x[0] > SCREEN_WIDTH - UNIT_SIZE) {
                 running = false;
             }
             //check if head touches top border
@@ -134,7 +179,7 @@ public class GamePanel extends JPanel implements ActionListener {
                 running = false;
             }
             //check if head touches bottom border
-            if (y[0] > SCREEN_HEIGHT) {
+            if (y[0] > SCREEN_HEIGHT - UNIT_SIZE) {
                 running = false;
             }
             if (!running) {
@@ -153,6 +198,9 @@ public class GamePanel extends JPanel implements ActionListener {
             move();
             checkApple();
             checkCollisions();
+            if (!running) {
+
+            }
         }
         repaint();
     }
@@ -191,18 +239,30 @@ public class GamePanel extends JPanel implements ActionListener {
                     if (speed > 0) {
                         speed -= 5;
                         newSpeed();
-                        scoreMultiplierFloat *= 1.13461; //score starts at 100 goes up to 1250
-                        System.out.println(speed);
+                        scoreMultiplierFloat *= 1.106f; //score starts at 100 goes up to 750
                     }
                     break;
                 case KeyEvent.VK_SUBTRACT: //slow down
-                    if (speed < 215) {
+                    if (speed < 200) {
                         speed += 5;
                         newSpeed();
-                        scoreMultiplierFloat /= 1.13461; //score stats at 100 goes down to 5
-                        System.out.println(speed);
+                        scoreMultiplierFloat /= 1.106f; //score stats at 100 goes down to 13
                     }
                     break;
+                case KeyEvent.VK_SPACE: //pause game
+                    if (timer.isRunning()) {
+                        speed = 100;
+                        newSpeed();
+                        timer.stop();
+                    } else {
+                        timer.start();
+                    }
+                    break;
+                case KeyEvent.VK_ENTER: { //start new game
+                    timer.stop();
+                    startGame();
+                }
+                break;
             }
         }
     }
